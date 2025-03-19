78ead1a0ed54a8b6e50620acd10058f91c92dff2
Harshavardhan <mamidipaka2003@gmail.com>
2025-03-19 19:33:12 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -3,14 +3,13 @@ export const Bio = {
   roles: [
     "Full Stack Developer",
     "AI/ML Developer",
-    "UI/UX Designer",
     "Programmer",
   ],
   description:
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/HarshaRockzz",
   resume:
-    "https://drive.google.com/file/d/1zws174OADQFGZ3BksDOnZOtlmNBEQgAP/view?usp=sharing",
+    "https://drive.google.com/file/d/1R9CVJPmbIfbiGaZN8R4stSLdBhnjr_20/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/mamidipaka-harshavardhan/",
   twitter: "https://twitter.com/",
   insta: "https://www.instagram.com/",
