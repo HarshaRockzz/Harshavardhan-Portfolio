8089fa0b70f019a2ae5d002e2f114350216b6b0e
Harshavardhan <mamidipaka2003@gmail.com>
2025-06-04 22:40:07 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -10,7 +10,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/HarshaRockzz",
   resume:
-    "https://drive.google.com/file/d/1o1wk-2NkUgKE4sZNaeLsYwguq8RRQgU5/view?usp=drive_link",
+    "",
   linkedin: "https://www.linkedin.com/in/mamidipaka-harshavardhan/",
   twitter: "https://twitter.com/",
   insta: "https://www.instagram.com/",
