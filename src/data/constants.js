8bc41608c6163653b68bc2f11985dd0201ee2c79
Harshavardhan <mamidipaka2003@gmail.com>
2025-10-31 22:48:38 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -10,7 +10,7 @@ export const Bio = {
     "I’m a driven and adaptable engineer with a passion for learning and solving real-world problems. I thrive in fast-paced environments and am committed to delivering high-impact, quality solutions.",
   github: "https://github.com/HarshaRockzz",
   resume:
-    "https://drive.google.com/file/d/1faL8Z-qG2fBX1NI-fB4W8d-AHAOw0eYZ/view?usp=sharing",
+    "https://drive.google.com/file/d/1mVpD2ljGkOVgxp0MSq8TYky8yOb46b7V/view?usp=drive_link",
   linkedin: "https://www.linkedin.com/in/mamidipaka-harshavardhan/",
   twitter: "https://twitter.com/",
   insta: "https://www.instagram.com/harshavardhan_2411/",
@@ -753,6 +753,7 @@ export const projects = [
     webapp: "https://github.com/HarshaRockzz/Face-Recognition-AI-with-Python",
   },
 ];
+
 
 
 
