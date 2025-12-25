69e1b3ae22ad2ad8bba92134f58ea095a1440860
Harshavardhan <mamidipaka2003@gmail.com>
2025-12-25 20:31:58 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -10,7 +10,7 @@ export const Bio = {
     "I’m a driven and adaptable engineer with a passion for learning and solving real-world problems. I thrive in fast-paced environments and am committed to delivering high-impact, quality solutions.",
   github: "https://github.com/HarshaRockzz",
   resume:
-    "https://drive.google.com/file/d/1XlHA5y126_Ixvzhbsybbqs8QEstJTYYE/view?usp=drive_link",
+    "https://drive.google.com/file/d/1pQno22ZoSpveLDq766qvy-stMu5VGvZY/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/mamidipaka-harshavardhan/",
   twitter: "https://twitter.com/",
   insta: "https://www.instagram.com/harshavardhan_2411/",
@@ -705,6 +705,7 @@ export const projects = [
     webapp: "https://github.com/HarshaRockzz/News-Classification-NLP"
   },
 ];
+
 
 
 
